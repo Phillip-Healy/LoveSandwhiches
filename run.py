e115144e431ce15cf5119a0618c39ebfe8dc3d75
Phillip-Healy <phillip.healy9@mail.dcu.ie>
2021-05-24 11:35:20 +0000

    added functionality to validate_data to check input type can be converted to integer and produce value error otherwise.

--- run.py
+++ run.py
@@ -12,6 +12,7 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
+
 def get_sales_data():
     """
     Get sales figures input from the user
@@ -25,12 +26,14 @@ def get_sales_data():
     sales_data = data_str.split(",")
     validate_data(sales_data)
 
+
 def validate_data(values):
     """
     Inside the try converts all string values to integers.
     Raises ValueError if strings cannot be converted or there aren't 6 values.
     """
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
